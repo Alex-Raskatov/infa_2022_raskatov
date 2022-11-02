33267b489a3c3398eddf5a5e9e87813202777a98
Alex-Raskatov <raskatov.study@gmail.com>
2022-11-02 19:44:52 +0300

    полностью переработан способ взаимодействия с фигурами, добавлена возможность создания множества фигур

--- lab8/ball.py
+++ lab8/ball.py
@@ -3,11 +3,15 @@ from pygame.draw import *
 from random import randint
 pygame.init()
 
+balls_counts_1 = int(input("Количество движущихся шаров: "))
+balls_counts_2 = int(input("Количество исчезающих шаров: "))
 FPS = 30
 Points = 0
 frame_1 = 0
 frame_2 = 0
 screen = pygame.display.set_mode((1200, 900))
+balls_1 = [[600, 450, randint(-5, 5), randint(-5, 5), 50, (randint(0, 255), randint(0, 255), randint(0, 255))] for i in range(balls_counts_1)]
+balls_2 = [[600, 450, randint(-5, 5), randint(-5, 5), 50, (randint(0, 255), randint(0, 255), randint(0, 255))] for i in range(balls_counts_2)]
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -18,52 +22,49 @@ CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
-x_1, x_2, y_1, y_2, r_1, r_2 = 600, 600, 450, 450, 50, 50
-v_x_1 = randint(-50, 50)/10
-v_y_1 = randint(-50, 50)/10
-
-def new_ball_1():
+def new_ball_1(number):
     '''рисует новый шарик, который движется '''
-    global x_1, y_1, r_1, v_x_1, v_y_1
-    x_1 += v_x_1
-    y_1 += v_y_1
-    r_1 += randint(-2, 2)
-    if x_1 < 0+r_1 or x_1 > 1200-r_1:
-        v_x_1 = - v_x_1 + randint(-2, 2)
-        v_y_1 += randint(-2, 2)
-    if y_1 > 900-r_1 or y_1 < 0+r_1:
-        v_y_1 = - v_y_1 + randint(-2, 2)
-        v_x_1 += randint(-2, 2)
-    color = COLORS[randint(0, 5)]
-    circle(screen, (200,50, 170), (x_1, y_1), r_1)
+    global balls_1
+    balls_1[number][0] += balls_1[number][2]
+    balls_1[number][1] += balls_1[number][3]
+    balls_1[number][4] += randint(-2, 2)
+    if balls_1[number][4] < 10: balls_1[number][4] = 10
+    if balls_1[number][4] > 100: balls_1[number][4] = 100
+    if (balls_1[number][0] < balls_1[number][4] and balls_1[number][2] < 0) or (balls_1[number][0] > 1200-balls_1[number][4] and balls_1[number][2] > 0):
+        balls_1[number][2] = - balls_1[number][2]
+        balls_1[number][3] = randint(-6, 6)
+    if (balls_1[number][1] > 900-balls_1[number][4] and balls_1[number][3] > 0) or (balls_1[number][1] < balls_1[number][4] and balls_1[number][3] < 0):
+        balls_1[number][3] = - balls_1[number][3]
+        balls_1[number][2] = randint(-6, 6)
+    circle(screen, balls_1[number][5], (balls_1[number][0], balls_1[number][1]), balls_1[number][4])
 
-def new_ball_2():
+def new_ball_2(number):
     '''рисует новый шарик, который исчезает '''
-    global x_2, y_2, r_2, frame_2
-    color = COLORS[randint(0, 5)]
-    if frame_2 == 60:
-        x_2 = randint(100, 1100)
-        y_2 = randint(100, 900)
-        r_2 = randint(10, 100)
-        circle(screen, (200, 50, 170), (x_2, y_2), r_2)
+    global balls_2, frame_2
+    if frame_2 == 30:
+        balls_2[number][0] = randint(100, 1100)
+        balls_2[number][1] = randint(100, 900)
+        balls_2[number][4] = randint(10, 100)
+        circle(screen, balls_2[number][5], (balls_2[number][0], balls_2[number][1]), balls_2[number][4])
         frame_2 = 0
     else:
         frame_2 += 1
-        circle(screen, (200, 170, 50), (x_2, y_2), r_2)
+        circle(screen, balls_2[number][5], (balls_2[number][0], balls_2[number][1]), balls_2[number][4])
 
-def click(event):
+def click_1(event, number):
     '''обработа попаданий и промахиваний по мячу'''
-    global  frame_2
-    if (event.pos[0]-x_1)**2 + (event.pos[1]-y_1)**2 < r_1**2:
+    global balls_1
+    if (event.pos[0] - balls_1[number][0])**2 + (event.pos[1] - balls_1[number][1])**2 < balls_1[number][4]**2:
         print("Ура!!!")
         points_1(True)
-    if (event.pos[0]-x_2)**2 + (event.pos[1]-y_2)**2 < r_2**2:
+
+def click_2(event, number):
+    '''обработа попаданий и промахиваний по мячу'''
+    global balls_2
+    if (event.pos[0] - balls_2[number][0])**2 + (event.pos[1] - balls_2[number][1])**2 < balls_2[number][4]**2:
         print("Ура!!!")
-        points_2(True)
-        frame_2 = 60
-    if (event.pos[0]-x_1)**2 + (event.pos[1]-y_1)**2 > r_1**2 and (event.pos[0]-x_2)**2 + (event.pos[1]-y_2)**2 > r_2**2:
-        print("Не ура")
-        points_1(False)
+        points_1(True)
+
 
 def points_1(flag):
     '''счетчик очков за движущийся шарик'''
@@ -102,9 +103,14 @@ while not finished:
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            click(event)
-    new_ball_2()
-    new_ball_1()
+            for i in range(balls_counts_1):
+                click_1(event, i)
+            for i in range(balls_counts_2):
+                click_2(event, i)
+    for i in range(balls_counts_1):
+        new_ball_1(i)
+    for i in range(balls_counts_2):
+        new_ball_2(i)
     img = font.render('Points: ' + str(Points), True, (255, 255, 255))
     screen.blit(img, (20, 20))
     pygame.display.update()
